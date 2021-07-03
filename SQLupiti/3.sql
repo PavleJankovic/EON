6c0c77e960f3039a7f623059d030386c5ea14d8f
PavleJankovic <86628222+PavleJankovic@users.noreply.github.com>
2021-07-04 00:23:52 +0200

    Update 3.sql

--- SQLupiti/3.sql
+++ SQLupiti/3.sql
@@ -1,1 +1,5 @@
- 
+ SELECT customer_id, profile_name, year_of_birth FROM Profile  AS A
+JOIN 
+	(SELECT customer_id AS custid, COUNT(*) AS cnt FROM Profile GROUP BY customer_id HAVING cnt > 1) AS B
+	ON A.customer_id = B.custid
+WHERE A.year_of_birth <= (YEAR(CURDATE()) - 65)
